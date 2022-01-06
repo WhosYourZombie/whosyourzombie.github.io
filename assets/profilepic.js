12dd3821ef4154f42f6e651467d9dd2def86a6f4
WhosYourZombie <jkreger@kent.edu>
2022-01-05 21:22:53 -0500

    update

--- assets/profilepic.js
+++ assets/profilepic.js
@@ -1,4 +1,4 @@
 $(function() {
-    var images = ['algometeo82.gif', 'algometeo88.gif', 'algoparrot.jpg', 'soulpod14.gif', 'pixelducky512.png'];
+    var images = ['algometeo82.gif', 'algometeo88.gif', 'algoparrot.jpg', 'soulpod014.gif', 'pixelducky512.png'];
     $('#background').css({'background-image': 'url(pics/' + images[Math.floor(Math.random() * images.length)] + ')'});
 });
